977eae6b715a52144aa0f25411cdc47870a73b39
Pranshoo Rathore <pranshoorathore@gmail.com>
2024-10-17 11:52:56 +0530

    Updated basic template

--- config/MongoDB.js
+++ config/MongoDB.js
@@ -1,5 +1,13 @@
 import { connect } from 'mongoose';
 
-const connectDB = () => {
-    const connection = connect();
+const connectDB = async () => {
+    try {
+        const connection = await connect(process.env.MONGO_URI);
+        console.log(`Database connected to ${connection.connection.host}`);    
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 }
+
+export default connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/MongoDB.js';
 
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+
+connectDB();
 
 app.listen(port, ()=>{
     console.log(`Server started at port ${port}`);
