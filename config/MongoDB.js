b3d39cc1c0ee431c3cc61198b4b650f7f53d7d5c
Pranshoo Rathore <pranshoorathore@gmail.com>
2024-10-17 11:47:48 +0530

    Node basic boilerplate

---
+++ config/MongoDB.js
@@ -0,0 +1,5 @@
+import { connect } from 'mongoose';
+
+const connectDB = () => {
+    const connection = connect();
+}
---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.listen(port, ()=>{
+    console.log(`Server started at port ${port}`);
+});
