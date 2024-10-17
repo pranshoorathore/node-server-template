e5579121b309d3dfb0767ef031a0af7d3c7f4dc6
Pranshoo Rathore <pranshoorathore@gmail.com>
2024-10-17 12:07:10 +0530

    Added ejs to template

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/MongoDB.js';
+import ejs from 'ejs';
 
 dotenv.config();
 
@@ -9,6 +10,13 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 connectDB();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.render('home');
+});
 
 app.listen(port, ()=>{
     console.log(`Server started at port ${port}`);
